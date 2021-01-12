2ccb1a451e602ba7582535fb138998f88b7f3851
GoRoX98 <39920128+GoRoX98@users.noreply.github.com>
2021-01-12 19:24:43 +0300

    0.0.0.14
    Первые подготовления к реализации строительства
    Незначительные правки в структуре проекта

---
+++ Assets/Scripts/Player/Economy/StructBuild.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct StructBuild
+{
+
+    private int food;
+    private int gold;
+    private int people;
+    private float life;
+
+}
